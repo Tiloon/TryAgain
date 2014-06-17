f11749f3b20513c1052a8db1f6146c8c0ebbe50f
Pim Zero <g33k.pim+zewor@gmail.com>
2014-06-17 21:30:44 +0200

    J'ai jamais passé autant de temps de ma vie sur des boutons.

--- TryAgain/TryAgain/TryAgain/GameStates/OptionScreen.cs
+++ TryAgain/TryAgain/TryAgain/GameStates/OptionScreen.cs
@@ -30,52 +30,7 @@ namespace TryAgain.GameStates
 
         Tuple<cButton, Vector2>[] resolutions;
 
-        private static Vector2[] reslist = 
-        {
-            new Vector2(800, 600),
-            new Vector2(1024, 768),
-            new Vector2(1360, 768),
-            new Vector2(1280, 800),
-            new Vector2(1280, 1024),
-            new Vector2(1400, 1050),
-            new Vector2(1600, 900),
-            new Vector2(1600, 1200),
-            new Vector2(1920, 1080),
-            new Vector2(1920, 1200),
-            new Vector2(2048, 1080),
-            new Vector2(2048, 1152),
-            new Vector2(2048, 1536),
-            new Vector2(2560, 1440),
-            new Vector2(2560, 1600),
-            new Vector2(2880, 1800),
-            new Vector2(3200, 2048),
-            new Vector2(3200, 2400)
-        };
-
         static public bool fullscreen = false;
-
-        private void loadResoltutions(GraphicsDevice graphics)
-        {
-            if (resolutions == null)
-            {
-                List<Vector2> l = new List<Vector2>();
-                foreach (Vector2 resolution in reslist)
-                {
-                    if ((Game1.graphics.GraphicsDevice.Adapter.CurrentDisplayMode.Height >= resolution.Y) &&
-                        (Game1.graphics.GraphicsDevice.Adapter.CurrentDisplayMode.Width >= resolution.X))
-                        l.Add(resolution);
-                }
-                resolutions = new Tuple<cButton, Vector2>[l.Count];
-                for (int i = 0; i < l.Count; i++)
-                {
-                    //cButton button = new cButton(Textures.GetStringTexture(Textures.Cache["UIBreturn"], l[i].X + "x" + l[i].Y, new Rectangle(0, 0, 128, 128)), Game1.graphics.GraphicsDevice);
-
-                    // Affiche la même chose que le bouton return
-                    resolutions[i] = new Tuple<cButton, Vector2>(new cButton(Textures.Cache["UIBreturn"], graphics), l[i]);
-                    resolutions[i].Item1.SetPosition(new Vector2(800, 600));
-                }
-            }
-        }
 
 
         public OptionScreen()
@@ -211,13 +166,6 @@ namespace TryAgain.GameStates
             buttonEnglish.Draw(sb);
             buttonFrancais.Draw(sb);
 
-            // La personne qui arrive à afficher quelque chose grâce à 
-            resolutions[0].Item1.Draw(sb); // ça,
-            foreach (Tuple<cButton, Vector2> resbutton in resolutions)
-            {
-                resbutton.Item1.Draw(sb); // ou encore mieux ça,
-            }
-            // je lui dit gros GG.
             // Normalement ça devrait afficher EXACTEMENT la même chose que le bouton return (voir au dessus)
         }
         public override void init(GraphicsDevice graphics)
@@ -254,7 +202,6 @@ namespace TryAgain.GameStates
             buttonEnglish.SetPosition(new Vector2(600, 400));
             buttonFrancais = new cButton(Textures.francais, graphics);
             buttonFrancais.SetPosition(new Vector2(600, 600));
-            loadResoltutions(graphics);
         }
     }
 }
