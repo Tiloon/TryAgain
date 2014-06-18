990cf27cccaecf8253746d67a2a287dc184e84fd
Totokh <tonytruanth@gmail.com>
2014-06-18 22:02:30 +0200

    craft

--- TryAgain/TryAgain/TryAgain/GameStates/GameScreen.cs
+++ TryAgain/TryAgain/TryAgain/GameStates/GameScreen.cs
@@ -151,6 +151,7 @@ namespace TryAgain.GameStates
                     if (newState.IsKeyDown(Keys.C))
                     {
                         Craft.Draw(sb);
+                        //Craft.Update();
                     }
                     Skills.Draw(sb, hero.getPosition(), hero, newState);
                 }
