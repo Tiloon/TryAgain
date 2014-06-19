76cf756e093dbc02cffef475d36f9f5ec912e195
Totokh <tonytruanth@gmail.com>
2014-06-19 13:00:38 +0200

    ressources remasterisees

--- TryAgain/TryAgain/TryAgain/GameElements/CraftInterface.cs
+++ TryAgain/TryAgain/TryAgain/GameElements/CraftInterface.cs
@@ -82,7 +82,7 @@ namespace TryAgain.GameElements
         {
             if (test == Craftpossible.potion) //Potion = 2 feu + branche
             {
-                if (Ressourceslist.quantity[0] < 1 && Ressourceslist.quantity[10] < 2)
+                if (Ressourceslist.quantity[0] < 1 || Ressourceslist.quantity[10] < 2)
                     return false;
                 else
                 {
@@ -94,7 +94,7 @@ namespace TryAgain.GameElements
             }
             else if (test == Craftpossible.bouclier) //Bouclier = 1 pierre + 1 bout de carton + 1 fer
             {
-                if (Ressourceslist.quantity[4] < 1 && Ressourceslist.quantity[5] < 1 && Ressourceslist.quantity[9] < 1)
+                if (Ressourceslist.quantity[4] < 1 || Ressourceslist.quantity[5] < 1 || Ressourceslist.quantity[9] < 1)
                     return false;
                 else
                 {
@@ -107,7 +107,7 @@ namespace TryAgain.GameElements
             }
             else if (test == Craftpossible.gun1)  //gun1 = 1 poney + 1 fer + 1 terre
             {
-                if (Ressourceslist.quantity[8] < 1 && Ressourceslist.quantity[5] < 1 && Ressourceslist.quantity[1] < 1)
+                if (Ressourceslist.quantity[8] < 1 || Ressourceslist.quantity[5] < 1 || Ressourceslist.quantity[1] < 1)
                     return false;
                 else
                 {
@@ -121,7 +121,7 @@ namespace TryAgain.GameElements
 
             else if (test == Craftpossible.bottes)
             {
-                if (Ressourceslist.quantity[3] < 1 && Ressourceslist.quantity[6] < 2)
+                if (Ressourceslist.quantity[3] < 1 || Ressourceslist.quantity[6] < 2)
                     return false;
                 else
                 {
@@ -134,7 +134,7 @@ namespace TryAgain.GameElements
 
             else if (test == Craftpossible.coca)
             {
-                if (Ressourceslist.quantity[10] < 1 && Ressourceslist.quantity[8] < 2)
+                if (Ressourceslist.quantity[10] < 1 || Ressourceslist.quantity[8] < 2)
                     return false;
                 else
                 {
@@ -147,7 +147,7 @@ namespace TryAgain.GameElements
 
             else if (test == Craftpossible.particulecannon)
             {
-                if (Ressourceslist.quantity[2] < 1 && Ressourceslist.quantity[6] < 1 && Ressourceslist.quantity[7] < 1)
+                if (Ressourceslist.quantity[2] < 1 || Ressourceslist.quantity[6] < 1 || Ressourceslist.quantity[7] < 1)
                     return false;
                 else
                 {
@@ -173,27 +173,27 @@ namespace TryAgain.GameElements
             else
                 sb.DrawString(Textures.UIfont, "Ressources                   Artisanat             Recette                Effet", new Vector2(Tilemap.variationsizegraphicsX, 120), Color.Blue);
             sb.Draw(Textures.O, new Rectangle(Tilemap.variationsizegraphicsX, 160, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[0].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 160), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[0].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 160), Color.White);
             sb.Draw(Textures.I, new Rectangle(Tilemap.variationsizegraphicsX, 200, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[1].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 200), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[1].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 200), Color.White);
             sb.Draw(Textures.II, new Rectangle(Tilemap.variationsizegraphicsX, 240, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[2].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 240), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[2].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 240), Color.White);
             sb.Draw(Textures.III, new Rectangle(Tilemap.variationsizegraphicsX, 280, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[3].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 280), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[3].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 280), Color.White);
             sb.Draw(Textures.IV, new Rectangle(Tilemap.variationsizegraphicsX, 320, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[4].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 320), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[4].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 320), Color.White);
             sb.Draw(Textures.V, new Rectangle(Tilemap.variationsizegraphicsX, 360, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[5].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 360), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[5].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 360), Color.White);
             sb.Draw(Textures.VI, new Rectangle(Tilemap.variationsizegraphicsX, 400, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[6].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 400), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[6].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 400), Color.White);
             sb.Draw(Textures.VII, new Rectangle(Tilemap.variationsizegraphicsX, 440, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[7].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 440), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[7].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 440), Color.White);
             sb.Draw(Textures.VIII, new Rectangle(Tilemap.variationsizegraphicsX, 480, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[8].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 480), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[8].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 480), Color.White);
             sb.Draw(Textures.IX, new Rectangle(Tilemap.variationsizegraphicsX, 520, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[9].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 520), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[9].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 520), Color.White);
             sb.Draw(Textures.X, new Rectangle(Tilemap.variationsizegraphicsX, 560, 40, 40), Color.White);
-            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[10].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 560), Color.Black);
+            sb.DrawString(Textures.UIfont, "* " + Ressourceslist.quantity[10].ToString(), new Vector2(Tilemap.variationsizegraphicsX + 50, 560), Color.White);
 
             sb.Draw(Textures.c0potion, new Rectangle(315, 160, 40, 40), Color.White);
             sb.Draw(Textures.c1shield, new Rectangle(315, 200, 40, 40), Color.White);
@@ -215,7 +215,7 @@ namespace TryAgain.GameElements
                 sb.DrawString(Textures.UIfont, msg, new Vector2(355, 160 + 40 *i), Color.White);
             }
 
-            sb.DrawString(Textures.UIfont, "2 feu + branche", new Vector2(Tilemap.variationsizegraphicsX + 550, 160), Color.Black);
+            sb.DrawString(Textures.UIfont, "2*ress0 + ress10", new Vector2(Tilemap.variationsizegraphicsX + 560, 160), Color.Black);
         }
     }
 }
